bb31e8e93910cd6ca58901f61752f7da02805452
Mario Fleischhacker <mario.fleischhacker@gmail.com>
2024-12-07 08:01:53 +0100

    feat(day07): solving part 2

--- src/bin/07.rs
+++ src/bin/07.rs
@@ -1,22 +1,46 @@
 #![feature(array_windows)]
 
 use anyhow::{bail, Context};
+use std::fmt::{Debug, Formatter};
 use std::ops::{Add, Mul};
 
 advent_of_code::solution!(7);
 
 pub fn part_one(input: &str) -> Option<u64> {
     let equations = parse_input(input).unwrap();
-    Some(count_solvable(equations))
+    Some(count_solvable(equations, &[u64::add, u64::mul]))
 }
 
-const OPERATORS: [fn(u64, u64) -> u64; 2] = [u64::add, u64::mul];
-fn count_solvable(equations: Vec<Equation>) -> u64 {
+pub fn part_two(input: &str) -> Option<u64> {
+    let equations = parse_input(input).unwrap();
+    Some(count_solvable(equations, &[u64::add, u64::mul, concat]))
+}
+
+fn concat(first: u64, second: u64) -> u64 {
+    let factor = count_digits(second);
+    first * (10u64.pow(factor)) + second
+}
+
+#[inline]
+fn count_digits(n: u64) -> u32 {
+    if n == 0 {
+        return 1;
+    }
+    let mut count = 0;
+    let mut num = n;
+    while num > 0 {
+        num /= 10;
+        count += 1;
+    }
+    count
+}
+
+fn count_solvable(equations: Vec<Equation>, operators: &[fn(u64, u64) -> u64]) -> u64 {
     let mut sum_total_valid = 0;
     for equation in equations {
         let num_operands = equation.candidates.len() - 1;
         // println!("\n{equation:?}");
-        for fun_candidates in generate_permutations(&OPERATORS, num_operands) {
+        for fun_candidates in generate_permutations(operators, num_operands) {
             // print_formula(&equation, &fun_candidates);
             let mut can = equation.candidates.iter();
             let mut sum = *can.next().expect("expected the candidates to be not empty");
@@ -53,26 +77,29 @@ fn generate_permutations<T: Copy>(input: &[T], k: usize) -> Vec<Vec<T>> {
     result
 }
 
-#[allow(unused)]
-fn print_formula(equation: &Equation, fun_candidates: &[fn(u64, u64) -> u64]) {
-    let mut start = equation.candidates.iter();
-    let mut print = String::new();
-    print.push_str(&format!("{}", start.next().unwrap()));
-    for (fun, num) in fun_candidates.iter().zip(start) {
-        let fun = if fun == &(u64::add as fn(u64, u64) -> u64) {
-            "+"
-        } else if fun == &(u64::mul as fn(u64, u64) -> u64) {
-            "*"
-        } else {
-            panic!("unknown function");
-        };
-        print.push_str(&format!(" {} {}", fun, num));
-    }
-    println!("{}", print);
-}
+#[allow(dead_code)]
+struct FormatFormula<'a>(&'a Equation, &'a [fn(u64, u64) -> u64]);
 
-pub fn part_two(_: &str) -> Option<u64> {
-    None
+impl Debug for FormatFormula<'_> {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        let Self(equation, fun_candidates) = self;
+        let mut start = equation.candidates.iter();
+        write!(f, "{}", start.next().unwrap())?;
+        // print.push_str(&format!("{}", start.next().unwrap()));
+        for (fun, num) in fun_candidates.iter().zip(start) {
+            let fun = if fun == &(u64::add as fn(u64, u64) -> u64) {
+                "+"
+            } else if fun == &(u64::mul as fn(u64, u64) -> u64) {
+                "*"
+            } else if fun == &(concat as fn(u64, u64) -> u64) {
+                "||"
+            } else {
+                panic!("unknown function");
+            };
+            write!(f, " {} {}", fun, num)?;
+        }
+        Ok(())
+    }
 }
 
 #[derive(Debug)]
@@ -115,6 +142,16 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(11387));
+    }
+
+    #[test]
+    fn test_formula_printer() {
+        let equation = Equation {
+            total: 123,
+            candidates: vec![55, 14, 34, 99, 12],
+        };
+        let ff = FormatFormula(&equation, &[u64::add, u64::mul, concat, u64::add]);
+        assert_eq!("55 + 14 * 34 || 99 + 12", format!("{ff:?}"));
     }
 }
