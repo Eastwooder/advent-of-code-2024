159aa1dee45ca7ff467c01e150c1b60704724dd2
Mario Fleischhacker <mario.fleischhacker@gmail.com>
2024-12-08 08:37:08 +0100

    feat(day08): part 2 solved

--- src/bin/08.rs
+++ src/bin/08.rs
@@ -6,14 +6,15 @@ advent_of_code::solution!(8);
 
 pub fn part_one(input: &str) -> Option<u32> {
     let (map_size, antennas) = parse_input(input);
-    Some(calculate_antinodes(map_size, antennas))
+    Some(calculate_antinodes_pairwise(map_size, antennas))
 }
 
-pub fn part_two(_: &str) -> Option<u32> {
-    None
+pub fn part_two(input: &str) -> Option<u32> {
+    let (map_size, antennas) = parse_input(input);
+    Some(calculate_antinodes_unbound(map_size, antennas))
 }
 
-fn calculate_antinodes(map_size: MapSize, antennas: Antennas) -> u32 {
+fn calculate_antinodes_pairwise(map_size: MapSize, antennas: Antennas) -> u32 {
     // debug_map(map_size, &antennas, None);
     let mut groups = HashMap::new();
     for (&pos, &f) in &antennas {
@@ -36,6 +37,36 @@ fn calculate_antinodes(map_size: MapSize, antennas: Antennas) -> u32 {
             }
             if check_in_bounds(d2, map_size) {
                 antinodes.insert((d2.0 as u32, d2.1 as u32));
+            }
+        }
+    }
+    // debug_map(map_size, &antennas, Some(&antinodes));
+    antinodes.len() as u32
+}
+
+fn calculate_antinodes_unbound(map_size: crate::MapSize, antennas: crate::Antennas) -> u32 {
+    // debug_map(map_size, &antennas, None);
+    let mut groups = HashMap::new();
+    for (&pos, &f) in &antennas {
+        groups.entry(f).or_insert(HashSet::new()).insert(pos);
+    }
+    let mut antinodes = Antinodes::new();
+    for (_, poss) in groups {
+        for a in poss.iter().combinations(2) {
+            let (first, second) = (a[0], a[1]);
+            let distance = (
+                first.0 as i32 - second.0 as i32,
+                first.1 as i32 - second.1 as i32,
+            );
+            let mut d1 = (first.0 as i32, first.1 as i32);
+            while check_in_bounds(d1, map_size) {
+                antinodes.insert((d1.0 as u32, d1.1 as u32));
+                d1 = (d1.0 + distance.0, d1.1 + distance.1);
+            }
+            let mut d2 = (second.0 as i32, second.1 as i32);
+            while check_in_bounds(d2, map_size) {
+                antinodes.insert((d2.0 as u32, d2.1 as u32));
+                d2 = (d2.0 - distance.0, d2.1 - distance.1);
             }
         }
     }
@@ -71,7 +102,7 @@ fn parse_input(input: &str) -> (MapSize, Antennas) {
 }
 
 #[allow(unused)]
-fn debug_map(map_size: MapSize, antennas: &Antennas, antinodes: Option<&Antinodes>) {
+fn debug_map(map_size: MapSize, antennas: &Antennas, antinodes: Option<&HashSet<(X, Y)>>) {
     let mut hm = HashMap::new();
     for (&pos, &f) in antennas {
         hm.insert(pos, f);
@@ -97,6 +128,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(34));
     }
 }
