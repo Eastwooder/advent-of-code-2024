46539148f65d338384fce5ef16ab6c3080610f78
Mario Fleischhacker <mario.fleischhacker@gmail.com>
2024-12-08 08:15:58 +0100

    feat(day08): part 1 solved

---
+++ src/bin/08.rs
@@ -0,0 +1,102 @@
+use advent_of_code::map2d::print_map;
+use itertools::Itertools;
+use std::collections::{HashMap, HashSet};
+
+advent_of_code::solution!(8);
+
+pub fn part_one(input: &str) -> Option<u32> {
+    let (map_size, antennas) = parse_input(input);
+    Some(calculate_antinodes(map_size, antennas))
+}
+
+pub fn part_two(_: &str) -> Option<u32> {
+    None
+}
+
+fn calculate_antinodes(map_size: MapSize, antennas: Antennas) -> u32 {
+    // debug_map(map_size, &antennas, None);
+    let mut groups = HashMap::new();
+    for (&pos, &f) in &antennas {
+        groups.entry(f).or_insert(HashSet::new()).insert(pos);
+    }
+    // println!("{:?}", groups);
+    let mut antinodes = Antinodes::new();
+    for (_, poss) in groups {
+        for a in poss.iter().combinations(2) {
+            let (first, second) = (a[0], a[1]);
+            let distance = (
+                first.0 as i32 - second.0 as i32,
+                first.1 as i32 - second.1 as i32,
+            );
+            // println!("{a:?} distance: {distance:?}");
+            let d1 = (first.0 as i32 + distance.0, first.1 as i32 + distance.1);
+            let d2 = (second.0 as i32 - distance.0, second.1 as i32 - distance.1);
+            if check_in_bounds(d1, map_size) {
+                antinodes.insert((d1.0 as u32, d1.1 as u32));
+            }
+            if check_in_bounds(d2, map_size) {
+                antinodes.insert((d2.0 as u32, d2.1 as u32));
+            }
+        }
+    }
+    // debug_map(map_size, &antennas, Some(&antinodes));
+    antinodes.len() as u32
+}
+
+fn check_in_bounds((x, y): (i32, i32), map_size: MapSize) -> bool {
+    x >= 0 && x < map_size.0 as i32 && y >= 0 && y < map_size.1 as i32
+}
+
+type X = u32;
+type Y = u32;
+type MapSize = (X, Y);
+type Antenna = (X, Y);
+type Frequency = char;
+type Antennas = HashMap<Antenna, Frequency>;
+type Antinode = (X, Y);
+type Antinodes = HashSet<Antinode>;
+
+fn parse_input(input: &str) -> (MapSize, Antennas) {
+    let width = input.lines().next().unwrap().len() as _;
+    let height = input.lines().count() as _;
+    let mut antennas = Antennas::new();
+    for (y, line) in input.lines().enumerate() {
+        for (x, ch) in line.chars().enumerate() {
+            if matches!(ch, '0'..='9' | 'a'..='z' | 'A'..='Z') {
+                antennas.insert((x as _, y as _), ch);
+            }
+        }
+    }
+    ((width, height), antennas)
+}
+
+#[allow(unused)]
+fn debug_map(map_size: MapSize, antennas: &Antennas, antinodes: Option<&Antinodes>) {
+    let mut hm = HashMap::new();
+    for (&pos, &f) in antennas {
+        hm.insert(pos, f);
+    }
+    if let Some(antinodes) = antinodes {
+        for &antinode in antinodes {
+            hm.insert(antinode, '#');
+        }
+    }
+    print_map(map_size, '.', &hm);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        let result = part_one(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, Some(14));
+    }
+
+    #[test]
+    fn test_part_two() {
+        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, None);
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,22 @@
 pub mod template;
 
 // Use this file to add helper functions and additional modules.
+
+pub mod map2d {
+    pub fn print_map(
+        map_size: (u32, u32),
+        empty: char,
+        poi: &std::collections::HashMap<(u32, u32), char>,
+    ) {
+        for y in 0..map_size.1 {
+            for x in 0..map_size.0 {
+                if let Some(p) = poi.get(&(x, y)) {
+                    print!("{}", p);
+                } else {
+                    print!("{}", empty);
+                }
+            }
+            println!();
+        }
+    }
+}
