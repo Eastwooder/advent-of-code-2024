0408a0957634491d3446861d133585ef65edb6ed
Mario Fleischhacker <mario.fleischhacker@gmail.com>
2024-12-14 13:57:58 +0100

    feat(day13): adapted for part2 but not solved yet

--- src/bin/13.rs
+++ src/bin/13.rs
@@ -1,18 +1,19 @@
-use glam::IVec2;
 use regex::Regex;
 
 advent_of_code::solution!(13);
 
-pub fn part_one(input: &str) -> Option<u32> {
+pub fn part_one(input: &str) -> Option<Cost> {
     let config = parse_input(input);
     Some(calculate_price(config))
 }
 
-pub fn part_two(_: &str) -> Option<u32> {
-    None
+pub fn part_two(input: &str) -> Option<Cost> {
+    let config = parse_input(input);
+    Some(calculate_price(config))
 }
 
-type Pos = IVec2;
+type Pos = glam::i64::I64Vec2;
+type Cost = u64;
 
 #[derive(Copy, Clone, Debug)]
 struct Configuration {
@@ -21,22 +22,22 @@ struct Configuration {
     price: Pos,
 }
 
-fn calculate_price(configs: Vec<Configuration>) -> u32 {
+fn calculate_price(configs: Vec<Configuration>) -> Cost {
     configs
         .iter()
         .map(|config| calculate_config(config, 3, 1))
         .sum()
 }
 
-fn calculate_config(config: &Configuration, cost_a: u32, cost_b: u32) -> u32 {
+fn calculate_config(config: &Configuration, cost_a: Cost, cost_b: Cost) -> Cost {
     let perp_a_b = config.a.perp_dot(config.b);
     let perp_a_price = config.a.perp_dot(config.price);
     let perp_price_b = config.price.perp_dot(config.b);
     if perp_a_b == 0 || perp_a_price % perp_a_b != 0 || perp_price_b % perp_a_b != 0 {
         0
     } else {
-        let num_press_a = (perp_price_b / perp_a_b) as u32;
-        let num_press_b = (perp_a_price / perp_a_b) as u32;
+        let num_press_a = (perp_price_b / perp_a_b) as Cost;
+        let num_press_b = (perp_a_price / perp_a_b) as Cost;
         (num_press_a * cost_a) + (num_press_b * cost_b)
     }
 }
@@ -66,13 +67,17 @@ mod tests {
 
     #[test]
     fn test_part_one() {
-        let result = part_one(&advent_of_code::template::read_file("examples", DAY));
+        let result = part_one(&advent_of_code::template::read_file_part(
+            "examples", DAY, 1,
+        ));
         assert_eq!(result, Some(480));
     }
 
     #[test]
     fn test_part_two() {
-        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        let result = part_two(&advent_of_code::template::read_file_part(
+            "examples", DAY, 2,
+        ));
+        assert_eq!(result, Some(875318608908));
     }
 }
